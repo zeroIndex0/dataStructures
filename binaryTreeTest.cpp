99cf55d3ecbcb2ba1f3e0feeed4e0783919b4dae
Ryan <RyanK.BusinessMail@gmail.com>
2018-03-10 13:51:45 -0500

    in-order, post-order, and pre-order traversal added to the test

--- binaryTreeTest.cpp
+++ binaryTreeTest.cpp
@@ -6,9 +6,9 @@
 
 #include <iostream>
 #include "binaryTree.h"
-
-
 using namespace std;
+
+
 
 
 //binary tree insert algorithm
@@ -30,6 +30,46 @@ void btInsert(BinaryTree<int> *t, int data) {
     }
 
 }
+
+
+//an interesting observation: the only thing that changes in these three functions is the location of the cout
+
+//in-order traversal
+void inOrder(BinaryTree<int> *t) {
+    if(t) {
+        if(t->getLeft())
+            inOrder(t->getLeft());
+        cout << t->getData() << " ";
+        if(t->getRight())
+            inOrder(t->getRight());
+    }
+}
+
+//post-order traversal
+void postOrder(BinaryTree<int> *t) {
+    if(t) {
+        if(t->getLeft())
+            postOrder(t->getLeft());  //LOL i left inOrder in the fucking post order function XD
+        if(t->getRight())
+            postOrder(t->getRight());
+        cout << t->getData() << " ";
+    }
+}
+
+//pre-order traversal
+void preOrder(BinaryTree<int> *t) {
+    if(t) {
+        cout << t->getData() << " ";
+        if(t->getLeft())
+            preOrder(t->getLeft());
+        if(t->getRight())
+            preOrder(t->getRight());
+
+    }
+}
+
+
+
 
 
 int main(void) {
@@ -58,9 +98,9 @@ int main(void) {
     cout << "\nOkay, lets do a little search! \nWhat number are you looking for? ";
     cin >> option;
 
-    BinaryTree<int> *searchNodes = NULL;
+    BinaryTree<int> *searchNodes = nullptr;
     searchNodes = t->search(option);
-    if(searchNodes == NULL) {
+    if(searchNodes == nullptr) {
         cout << option << " is not in the tree" << endl;
     } else {
         cout << "Found " << searchNodes->getData() << endl;
@@ -70,6 +110,21 @@ int main(void) {
 
     //Testing out the getHeight function added to binaryTree.h
     cout << "The tree height is: " << t->getHeight() << endl;
+
+
+
+    // ============== Testing Out Tree Traversals ================\\
+
+    cout << "In Order Traversal: " << endl;
+    inOrder(t);
+
+    cout << endl << "Post Order Traversal: " << endl;
+    postOrder(t);
+
+    cout << endl << "Pre Order Traversal: " << endl;
+    preOrder(t);
+
+
 
 return 0;
 }
